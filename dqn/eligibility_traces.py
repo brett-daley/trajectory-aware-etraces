d4eb7b03c845eeb6d7082e144b989d65b20bcb4b
Brett Daley <brett-daley@users.noreply.github.com>
2021-07-18 03:20:00 -0400

    Another bug fix in Moretrace

--- dqn/eligibility_traces.py
+++ dqn/eligibility_traces.py
@@ -15,8 +15,6 @@ class OfflineEligibilityTrace(ABC):
         self._reset()
 
     def _reset(self):
-        self._trace_products = np.zeros(self._maxlen, dtype=np.float64)
-        self._discounting = np.zeros(self._maxlen, dtype=np.float64)
         self._eligibility = np.zeros(self._maxlen, dtype=np.float64)
         self._updates = np.zeros(self._maxlen, dtype=np.float64)
 
@@ -28,18 +26,13 @@ class OfflineEligibilityTrace(ABC):
 
         sl = slice(self._current_episode_start, self._t)
 
-        self._trace_products[sl] *= self._trace_coefficient(target_prob, behavior_prob)
-        self._discounting[sl] *= self._discount
-        self._eligibility[sl] *= self._lambd
-
-        self._trace_products[self._t] += 1.0
-        self._discounting[self._t] += 1.0
+        trace = self._trace_coefficient(target_prob, behavior_prob)
+        self._eligibility[sl] *= self._discount * trace
         self._eligibility[self._t] += 1.0
 
         sl = slice(self._current_episode_start, self._t + 1)
 
-        self._updates[sl] += td_error * self._discounting[sl] * self._eligibility[sl] \
-            * self._get_trace_products(sl)
+        self._updates[sl] += td_error * self._eligibility[sl]
 
         self._t += 1
 
@@ -50,9 +43,6 @@ class OfflineEligibilityTrace(ABC):
     def _trace_coefficient(self, target_prob, behavior_prob):
         raise NotImplementedError
 
-    def _get_trace_products(self, sl):
-        return self._trace_products[sl]
-
     def get_updates_and_reset(self):
         updates = self._updates[:self._t].copy()
         self._reset()
@@ -62,30 +52,56 @@ class OfflineEligibilityTrace(ABC):
 class IS(OfflineEligibilityTrace):
     def _trace_coefficient(self, target_prob, behavior_prob):
         assert behavior_prob > 0.0
-        return target_prob / behavior_prob
+        return self._lambd * target_prob / behavior_prob
 
 
 class Qlambda(OfflineEligibilityTrace):
     def _trace_coefficient(self, target_prob, behavior_prob):
-        return 1.0
+        return self._lambd
 
 
 class TB(OfflineEligibilityTrace):
     def _trace_coefficient(self, target_prob, behavior_prob):
-        return target_prob
+        return self._lambd * target_prob
 
 
 class Retrace(OfflineEligibilityTrace):
     def _trace_coefficient(self, target_prob, behavior_prob):
         assert behavior_prob > 0.0
-        return min(1.0, target_prob / behavior_prob)
+        return self._lambd * min(1.0, target_prob / behavior_prob)
 
 
-class Moretrace(IS):
-    def _get_trace_products(self, sl):
-        return np.minimum(1.0, self._trace_products[sl])
+class Moretrace(OfflineEligibilityTrace):
+    def _reset(self):
+        super()._reset()
+        self._eligibility = None
+        self._trace_products = np.zeros(self._maxlen, dtype=np.float64)
+        self._discounting = np.zeros(self._maxlen, dtype=np.float64)
+        self._lambda_products = np.zeros(self._maxlen, dtype=np.float64)
 
+    def update(self, td_error, target_prob, behavior_prob, done):
+        assert self._t < self._maxlen
 
-class Safetrace(IS):
-    def _get_trace_products(self, sl):
-        return np.minimum(self._eligibility[sl], self._trace_products[sl])
+        sl = slice(self._current_episode_start, self._t)
+
+        self._trace_products[sl] *= self._trace_coefficient(target_prob, behavior_prob)
+        self._discounting[sl] *= self._discount
+        self._lambda_products[sl] *= self._lambd
+
+        self._trace_products[self._t] += 1.0
+        self._discounting[self._t] += 1.0
+        self._lambda_products[self._t] += 1.0
+
+        sl = slice(self._current_episode_start, self._t + 1)
+
+        self._updates[sl] += td_error * self._discounting[sl] \
+            * np.minimum(self._lambda_products[sl], self._trace_products[sl])
+
+        self._t += 1
+
+        if done:
+            self._current_episode_start = self._t
+
+    def _trace_coefficient(self, target_prob, behavior_prob):
+        assert behavior_prob > 0.0
+        return target_prob / behavior_prob
