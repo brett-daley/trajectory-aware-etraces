d00b7ff536282577be38dda6564a6c0fe14fc602
Brett Daley <brett-daley@users.noreply.github.com>
2021-11-16 20:02:06 -0500

    Moretrace tweak

--- dqn/experience_replay/eligibility_traces.py
+++ dqn/experience_replay/eligibility_traces.py
@@ -66,6 +66,11 @@ class Retrace(OfflineEligibilityTrace):
 
 
 class Moretrace(OfflineEligibilityTrace):
+    def __init__(self, discount, lambd, p=0.5):
+        super().__init__(discount, lambd)
+        assert 0.0 <= p <= 1.0
+        self._p = p
+
     def __call__(self, td_errors, behavior_probs, target_probs, dones):
         assert (behavior_probs > 0.0).all()
 
@@ -75,26 +80,25 @@ class Moretrace(OfflineEligibilityTrace):
             assert L == len(array)
 
         # The eligibility has been split into subcomponents for efficiency
-        discount_products = np.zeros(L)
-        lambda_products = np.zeros(L)
-        isratio_products = np.zeros(L)
+        decay_products = np.ones(L)
+        isratio_products = np.ones(L)
+        retrace_products = np.ones(L)
         updates = np.zeros(L)
 
         current_episode_start = 0
         for t in range(L):  # For each timestep in the trajectory:
             # Decay all past eligibilities
             sl = slice(current_episode_start, t)
-            discount_products[sl] *= self._discount
-            lambda_products[sl] *= self._lambd
-            isratio_products[sl] *= (target_probs[t] / behavior_probs[t])
-
-            # Increment eligibility of current timestep
-            for array in [discount_products, lambda_products, isratio_products]:
-                array[t] += 1.0
+            decay_products[sl] *= self._discount * self._lambd
+            ratio = target_probs[t] / behavior_probs[t]
+            isratio_products[sl] *= ratio
+            retrace_products[sl] *= min(1.0, ratio)
 
             # Apply current TD error to all past/current timesteps in proportion to eligibilities
             sl = slice(current_episode_start, t + 1)
-            eligibility = discount_products[sl] * np.minimum(lambda_products[sl], isratio_products[sl])
+            eligibility = decay_products[sl] \
+                            * np.minimum(1.0, isratio_products[sl]) ** self._p \
+                            * retrace_products[sl] ** (1.0 - self._p)
             updates[sl] += td_errors[t] * eligibility
 
             if dones[t]:
