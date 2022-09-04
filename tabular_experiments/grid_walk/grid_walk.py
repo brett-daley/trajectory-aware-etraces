d4686e0e8575334131fb397a645dab718658d433
Brett Daley <brett-daley@users.noreply.github.com>
2022-09-03 19:58:31 -0600

    GridWalk modifications

--- tabular_experiments/grid_walk/grid_walk.py
+++ tabular_experiments/grid_walk/grid_walk.py
@@ -1,10 +1,19 @@
-from gym_classics.envs.abstract.noisy_gridworld import NoisyGridworld
+from gym_classics.envs.abstract.gridworld import Gridworld
 
 
-class GridWalk(NoisyGridworld):
+class GridWalk(Gridworld):
     def __init__(self):
-        self._goal = (9, 3)
-        super().__init__(dims=(10, 7), starts={(0, 3)})
+        self._goal = (9, 1)
+        super().__init__(dims=(10, 3), starts={(0, 1)})
+
+    def _move(self, state, action):
+        x, y = state
+        return {
+            0: (x,   y+1),  # Up
+            1: (x+1, y),    # Right
+            2: (x,   y-1),  # Down
+            3: (x,   y)     # Instead of Left, do a No-op
+        }[action]
 
     def _reward(self, state, action, next_state):
         return 1.0 if self._done(state, action, next_state) else 0.0
