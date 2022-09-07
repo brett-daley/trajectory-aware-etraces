2251ee00af48415327735d8f5f4ced228e37b783
Brett Daley <brett-daley@users.noreply.github.com>
2022-09-07 11:01:10 -0600

    Bugfix in seed generator

--- tabular_experiments/learning_curves.py
+++ tabular_experiments/learning_curves.py
@@ -40,7 +40,7 @@ def postformat_plots():
 def generate_seeds(meta_seed, n):
     np_random = np.random.RandomState(meta_seed)  # Set seed for reproducibility
     # Randomize the seeds here to avoid any possible bias from reusing seeds from the hyperparameter search
-    return [np_random.randint(2**31) for _ in range(100)]
+    return [np_random.randint(2**31) for _ in range(n)]
 
 
 def plot_learning_curves(env_id, behavior_policy, target_policy, algo_specs, n_episodes, title):
