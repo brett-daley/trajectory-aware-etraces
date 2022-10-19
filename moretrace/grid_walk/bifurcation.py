417b60619b87adeb06d087145e43e72ebba59969
Brett Daley <brett-daley@users.noreply.github.com>
2022-10-19 01:45:15 -0400

    Deterministic bifurcation environment

--- moretrace/grid_walk/bifurcation.py
+++ moretrace/grid_walk/bifurcation.py
@@ -1,4 +1,4 @@
-from gym_classics.envs.abstract.noisy_gridworld import NoisyGridworld as Gridworld
+from gym_classics.envs.abstract.gridworld import Gridworld
 
 
 class Bifurcation(Gridworld):
@@ -12,15 +12,6 @@ class Bifurcation(Gridworld):
             (3,1), (3,2), (3,3)
         }
         super().__init__(n_actions=4, dims=(5, 5), starts={(0, 0)}, blocks=blocks)
-
-    def _move(self, state, action):
-        x, y = state
-        return {
-            0: (x,   y+1),  # Up
-            1: (x+1, y),    # Right
-            2: (x,   y-1),  # Down
-            3: (x-1, y)     # Left
-        }[action]
 
     def _reward(self, state, action, next_state):
         return 1.0 if self._done(state, action, next_state) else 0.0
